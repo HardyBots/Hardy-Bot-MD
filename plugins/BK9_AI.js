fe7fb7abd622c1394c73dfb1d6b61881ecadee04
Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>
2024-06-24 01:47:24 +0100

    🇲🇦 𝙷𝙰𝚁𝙳𝚈 - 𝙱𝙾𝚃 - 𝙼𝙳 👑

--- plugins/BK9_AI.js
+++ plugins/BK9_AI.js
@@ -8,7 +8,7 @@ const handler = async (m, {conn, text, command}) => {
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`));
   const tradutor = _translate.BK9.BK9;
 
-  if (command === 'bk9dalle') {
+  if (command === 'gxdalle') {
     if (!text) throw `${tradutor.bk9dalletext}`;
 
     await conn.sendMessage(m.chat, {text: tradutor.bk9dallewait}, {quoted: m});
@@ -24,7 +24,7 @@ const handler = async (m, {conn, text, command}) => {
     } catch (error) {
       throw `${tradutor.bk9dalleerr}`;
     }
-  } else if (command === 'bk9') {
+  } else if (command === 'gx') {
     if (!text) throw `${tradutor.bk9text}`;
 
     try {
@@ -41,7 +41,7 @@ const handler = async (m, {conn, text, command}) => {
     } catch (error) {
       throw `${tradutor.bk9err}`;
     }
-  } else if (command === 'bk9img') {
+  } else if (command === 'gximg') {
     let BK7 = m.quoted ? m.quoted : m;
     let BK8 = (BK7.msg || BK7).mimetype || BK7.mediaType || '';
     if (/image/g.test(BK8) && !/webp/g.test(BK8)) {
@@ -55,6 +55,6 @@ const handler = async (m, {conn, text, command}) => {
   }
 };
 
-handler.command = ['bk9dalle', 'bk9', 'bk9img'];
+handler.command = ['gxdalle', 'gx', 'gximg'];
 handler.tags = ['ai'];
 export default handler;
