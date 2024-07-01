461cdd0d02c1cca4c02f8175aaecc7fbd4e61955
Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>
2024-07-01 23:56:52 +0100

    🇲🇦 𝙷𝙰𝚁𝙳𝚈 - 𝙱𝙾𝚃 - 𝙼𝙳 👑

--- plugins/ai-gemini.js
+++ plugins/ai-gemini.js
@@ -37,12 +37,3 @@ const handler = async (m, {conn, text, command}) => {
 handler.command = ['gemini'];
 handler.tags = ['ai'];
 export default handler;
-
-
-let BK7 = m.quoted ? m.quoted : m;
-    let BK8 = (BK7.msg || BK7).mimetype || BK7.mediaType || '';
-    if (/image/g.test(BK8) && !/webp/g.test(BK8)) {
-      let BK0 = await BK7.download();
-      let BK9img = await uploader(BK0);
-      let BK9api = await (await fetch(`https://api.bk9.site/ai/geminiimg?url=${BK9img}&q=${text}`)).json();
-      conn.sendMessage(m.chat, { text: BK9api.BK9 }, { quoted: m });
