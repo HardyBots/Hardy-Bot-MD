e079300d49023b82125aafeec81a1f88952cb00f
Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>
2024-07-01 23:56:24 +0100

    🇲🇦 𝙷𝙰𝚁𝙳𝚈 - 𝙱𝙾𝚃 - 𝙼𝙳 👑

--- plugins/ai-gemini.js
+++ plugins/ai-gemini.js
@@ -8,33 +8,28 @@ const handler = async (m, {conn, text, command}) => {
   const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`));
   const tradutor = _translate.BK9.BK9;
 
-    if (command === 'bard') {
+    if (command === 'gemini') {
     if (!text) throw `${tradutor.bk9text}`;
 
     try {
-      conn.sendPresenceUpdate('composing', m.chat);
-      const BK9api = `https://api.bk9.site/ai/gemini?q=${encodeURIComponent(text)}`;
-      const BK99 = await fetch(BK9api);
-      const BK8 = await BK99.json();
-      if (BK8.status && BK8.BK9) {
-        const respuestaAPI = BK8.BK9;
-        conn.reply(m.chat, respuestaAPI, m);
-      } else {
-        throw `${tradutor.bk9err}`;
-      }
-    } catch (error) {
-      throw `${tradutor.bk9err}`;
-    }
-  } else if (command === 'bardimg') {
-    let BK7 = m.quoted ? m.quoted : m;
+      let BK7 = m.quoted ? m.quoted : m;
     let BK8 = (BK7.msg || BK7).mimetype || BK7.mediaType || '';
     if (/image/g.test(BK8) && !/webp/g.test(BK8)) {
       let BK0 = await BK7.download();
       let BK9img = await uploader(BK0);
       let BK9api = await (await fetch(`https://api.bk9.site/ai/geminiimg?url=${BK9img}&q=${text}`)).json();
       conn.sendMessage(m.chat, { text: BK9api.BK9 }, { quoted: m });
-    } else {
-      throw `${tradutor.bk9imgtext}`;
+      } else {
+        conn.sendPresenceUpdate('composing', m.chat);
+      const BK9api = `https://api.bk9.site/ai/gemini?q=${encodeURIComponent(text)}`;
+      const BK99 = await fetch(BK9api);
+      const BK8 = await BK99.json();
+      if (BK8.status && BK8.BK9) {
+        const respuestaAPI = BK8.BK9;
+        conn.reply(m.chat, respuestaAPI, m);
+      }
+    } catch (error) {
+      throw `${tradutor.bk9err}`;
     }
   }
 };
@@ -42,3 +37,12 @@ const handler = async (m, {conn, text, command}) => {
 handler.command = ['gemini'];
 handler.tags = ['ai'];
 export default handler;
+
+
+let BK7 = m.quoted ? m.quoted : m;
+    let BK8 = (BK7.msg || BK7).mimetype || BK7.mediaType || '';
+    if (/image/g.test(BK8) && !/webp/g.test(BK8)) {
+      let BK0 = await BK7.download();
+      let BK9img = await uploader(BK0);
+      let BK9api = await (await fetch(`https://api.bk9.site/ai/geminiimg?url=${BK9img}&q=${text}`)).json();
+      conn.sendMessage(m.chat, { text: BK9api.BK9 }, { quoted: m });
