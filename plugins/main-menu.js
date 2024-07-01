8c26adf6d2522de53f5bbcd49d8c0c5e0df569b6
Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>
2024-07-01 14:38:47 +0100

    🇲🇦 𝙷𝙰𝚁𝙳𝚈 - 𝙱𝙾𝚃 - 𝙼𝙳 👑

---
+++ plugins/main-menu.js
@@ -0,0 +1,68 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.menu_labiblia
+
+if (!db.data.chats[m.chat].modohorny && m.isGroup) throw `${tradutor.texto1[0]} ${usedPrefix}enable modohorny*`; 
+  try {
+    const pp = imagen1;
+    const vn = './media/menu.mp3';
+    const d = new Date(new Date + 3600000);
+    const locale = 'ar';
+    const week = d.toLocaleDateString(locale, {weekday: 'long'});
+    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+    const _uptime = process.uptime() * 1000;
+    const uptime = clockString(_uptime);
+    const user = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = global.db.data.users[m.sender];
+    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const more = String.fromCharCode(8206);
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
+║    ◉— *Ｈａｒｄｙ － Ｂｏｔ* —◉
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
+║➤  مرحبا ${taguser}*
+╰═══╡✯✯✯✯✯✯✯✯✯╞═══╯
+
+┏━━━━━━━━━━━━━━━━┓
+┃           𝔸𝕀 𝕄𝔼ℕ𝕌
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 🤖 _${usedPrefix}bard_
+┣ ඬ⃟ 🤖 _${usedPrefix}bardimg_
+┣ ඬ⃟ 🤖 _${usedPrefix}ia_
+┣ ඬ⃟ 🤖 _${usedPrefix}ia2_
+┣ ඬ⃟ 🤖 _${usedPrefix}gpt_
+┣ ඬ⃟ 🤖 _${usedPrefix}gpt2_
+┣ ඬ⃟ 🤖 _${usedPrefix}gpt3_
+┣ ඬ⃟ 🤖 _${usedPrefix}gpt4_
+┗━━━━━━━━━━━━━━━━┛`.trim();
+    if (m.isGroup) {
+      await conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+      await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, {type: 'audioMessage', ptt: true});
+    } else {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      await conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+      await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, {type: 'audioMessage', ptt: true});
+    }
+  } catch {
+    conn.reply(m.chat, tradutor.texto1[3], m);
+  }
+};
+handler.command = /^(menu|help|hardy)$/i;
+handler.exp = 50;
+handler.fail = null;
+export default handler;
+
+function clockString(ms) {
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
+}
