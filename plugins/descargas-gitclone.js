dce4be4f53c570d77abe7a8b359b647f54e4de67
Aymane Touzani <158565484+gxaymenofc@users.noreply.github.com>
2024-07-07 16:25:19 +0100

    👑 𝙴𝙻𝚅𝙴𝙽 - 𝙱𝙾𝚃 - 𝙼𝙳 🇲🇦

--- plugins/descargas-gitclone.js
+++ plugins/descargas-gitclone.js
@@ -1,22 +1,20 @@
-import fetch from 'node-fetch';
+import fetch from 'node-fetch'
+const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
+let handler = async (m, { args, usedPrefix, command }) => {
+    if (!args[0]) throw `Example user ${usedPrefix}${command} https://github.com/noureddineouafy/BOBIZA-BOT`
+    if (!regex.test(args[0])) throw 'wrong link!'
+    let [_, user, repo] = args[0].match(regex) || []
+    repo = repo.replace(/.git$/, '')
+    let url = `https://api.github.com/repos/${user}/${repo}/zipball`
+    let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
+    // 'attachment; filename=Nurutomo-wabot-aq-v2.5.1-251-g836cccd.zip'
+    m.reply(`*Please wait, sending repository..*\n*جاري تحميل المشروع المرجو الانتطار قليلا*`)
+    conn.sendFile(m.chat, url, filename, null, m)
+}
+handler.help = ['gitclone']
+handler.tags = ['downloader']
+handler.command = /gitclone/i
 
+handler.limit = false
 
-
-const regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i;
-const handler = async (m, {args, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.descargas_gitclone
-
-  if (!args[0]) throw `${tradutor.texto1} _${usedPrefix + command} https://github.com/BrunoSobrino/TheMystic-Bot-MD_`;
-  if (!regex.test(args[0])) throw tradutor.texto2;
-  let [_, user, repo] = args[0].match(regex) || [];
-  repo = repo.replace(/.git$/, '');
-  const url = `https://api.github.com/repos/${user}/${repo}/zipball`;
-  const filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1];
-  m.reply(tradutor.texto3);
-  conn.sendFile(m.chat, url, filename, null, m);
-};
-handler.command = /gitclone/i;
-export default handler;
+export default handler
